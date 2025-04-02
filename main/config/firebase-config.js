0172921f299bfb2cee7635a6da56c424c3afe9fd
RDinujaya4 <136170467+RDinujaya4@users.noreply.github.com>
2025-04-02 14:37:00 +0530

    New Firebase Database added

--- main/config/firebase-config.js
+++ main/config/firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAMbcaPACOEbCW6ci6R4rVgIcJv1GOdK5o",
-    authDomain: "banana-game-8dfcd.firebaseapp.com",
-    projectId: "banana-game-8dfcd",
-    storageBucket: "banana-game-8dfcd.firebasestorage.app",
-    messagingSenderId: "593299248539",
-    appId: "1:593299248539:web:aa9c118aceab2274fdbd3e"
+    apiKey: "AIzaSyDbhdF_1-tcnKIdeErme4C-iOFY1-JQ7iA",
+    authDomain: "banana-math-blitz-a0baa.firebaseapp.com",
+    projectId: "banana-math-blitz-a0baa",
+    storageBucket: "banana-math-blitz-a0baa.firebasestorage.app",
+    messagingSenderId: "142589278674",
+    appId: "1:142589278674:web:5dbd9453aef198455ff00b"
   };
   
   firebase.initializeApp(firebaseConfig);
